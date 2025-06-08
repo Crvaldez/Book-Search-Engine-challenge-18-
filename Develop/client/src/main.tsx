e7795cf4c39cdae9cbba0657ae03564836df4c93
Cristopher <cristophervaldezd@gmail.com>
2025-06-07 21:46:35 -0400

    configure Apollo Client in main.tsx and wrap RouterProvider with ApolloProvider

--- Develop/client/src/main.tsx
+++ Develop/client/src/main.tsx
@@ -1,11 +1,43 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import App from './App.jsx';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+
+// Set the GraphQL endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Add JWT to every request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// Set up routing
 const router = createBrowserRouter([
   {
     path: '/',
@@ -14,15 +46,21 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <SearchBooks />
-      }, {
+        element: <SearchBooks />,
+      },
+      {
         path: '/saved',
-        element: <SavedBooks />
-      }
-    ]
-  }
-])
+        element: <SavedBooks />,
+      },
+    ],
+  },
+]);
 
+// Render the app with Apollo and Router Providers
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-)
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
+  </React.StrictMode>
+);
